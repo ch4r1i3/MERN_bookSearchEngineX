b00cd2c30819ed0c106c531625034fb9e7ccb450
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-03-11 13:55:18 -0500

    apollo server added to server.js on server side

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+//import apollo server
+const { ApolloServer } = require("apollo-server-express");
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
+//db connection
+const db = require("./config/connection");
+
+// const routes = require('./routes');
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+//apply apollo server with express app
+server.applyMiddleware({ app });
+
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+//get all
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +16,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -29,7 +29,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
@@ -37,3 +37,41 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+// trial 2 
+
+// const jwt = require('jsonwebtoken');
+
+// // set token secret and expiration date
+// const secret = 'mysecretsshhhhh';
+// const expiration = '2h';
+
+// // Apollo Server Auth
+
+// module.exports = {
+//   signToken: function({ username, email, _id }) {
+//     const payload = { username, email, _id };
+
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+//   authMiddleware: function({ req }) {
+//     let token = req.body.token || req.query.token || req.headers.authorization;
+//     if (req.headers.authorization) {
+//         token = token
+//             .split(' ')
+//             .pop()
+//             .trim();
+//     }
+//     if (!token) {
+//         return req;
+//     }
+//     try {
+//         const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//         req.user = data;
+//     } catch {
+//         console.log('Invalid token');
+//     }
+//     // return the updated request object
+//     return req;
+//   }
+// };
