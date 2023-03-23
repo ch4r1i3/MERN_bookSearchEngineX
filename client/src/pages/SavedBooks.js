6643004065d247de6c29024ed991d6541488f05e
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-03-23 19:52:22 -0400

    update

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -72,7 +72,7 @@ const SavedBooks = () => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
-                  {book.link ? <Card.Text><a href={book.link} target="_blank">More Information on Google Books</a></Card.Text> : null}
+                  {book.link ? <Card.Text><a href={book.link} target="_blank" rel="noopener noreferrer">More Information on Google Books</a></Card.Text> : null}
                   <Card.Text>{book.description}</Card.Text>
                   <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book
